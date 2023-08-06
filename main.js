6be9092343b87648f175091cae7404753fb7343d
Santiago Garcia <santiago.garcia@posteo.de>
2023-08-06 17:54:33 +0200

    Mobile dialog

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import "virtual:uno.css";
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  document.getElementById("email-confirmation").innerText =
+    event.target.elements.email.value;
+  document.querySelector("dialog").showModal();
+});
+
+document
+  .getElementById("closeDialog")
+  .addEventListener("click", () => document.querySelector("dialog").close());
